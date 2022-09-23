b1b2a64d139acd616830b77b4b71f2b21165d7e1
Alexander Walther <aw@alexplus.de>
2022-09-23 19:56:04 +0200

    wording beauftragung

--- pages/settings.php
+++ pages/settings.php
@@ -20,7 +20,7 @@ $fragment->setVar('body', $form->get(), false);
 	<div class="col-lg-4">
 		<?php
 
-$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=wortpresse"><img src="'.rex_url::addonAssets('wortpresse', 'unterstuetzen.svg').'" style="width: 100% max-width: 400px;"></a>';
+$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=wortpresse"><img src="'.rex_url::addonAssets('wortpresse', 'jetzt-beauftragen.svg').'" style="width: 100% max-width: 400px;"></a>';
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'info', false);
