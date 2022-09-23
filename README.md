545d6f5e2703514fec4b8b47b14dad6cae7ed764
Alexander Walther <aw@alexplus.de>
2022-09-23 19:30:22 +0200

    Tabelle Kommentare, weitere Meta-Infos, README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Wortpresse - Beiträge, Kategorien, Taxonomien und Menüs
+# Wortpresse für REDAXO - Beiträge, Kategorien, Taxonomien, Menüs, Kommentare und mehr...
 
 Erweitert REDAXO um beliebte Funktionen eines bekannten Blogging-CMS.
 
@@ -11,14 +11,26 @@ Coming soon, das ist geplant:
 Erstelle Tags und weise sie Struktur-Elementen wie REDAXO-Kategorien und REDAXO-Artikeln zu - oder eigenen Addons und YForm-Tabellen!
 
 Erstelle auf Basis von Tags und dem URL-Addon eigene, SEO-optimierte Übersichtsseiten für Tags und benutze REDAXO-Artikel als Blog / News-Artikel.
+
 ### Kategorien
 
 Erstelle Kategorien und weise sie Struktur-Elementen wie REDAXO-Kategorien und REDAXO-Artikeln zu - oder eigenen Addons und YForm-Tabellen!
 
 Erstelle auf Basis von Tags und dem URL-Addon eigene, SEO-optimierte Übersichtsseiten für Kategorien und benutze REDAXO-Artikel als Blog / News-Artikel.
+
 ### Menüs
 
 Erstelle auf Basis von YForm-Tabellen eigene Menüs und nutze gemeinsam mit dem Addon "YForm Usability" die Drag-&-Drop-Funktionalität.
+
+### Autoren und Veröffentlichungsdatum / Status
+
+Weise Artikeln zusätzlich eigene Autoren und ein Veröffentlichungsdatum zu, welche die REDAXO-eigene Funktion überschreibt.
+
+### Kommentare
+
+Erstelle auf Basis von YForm eigene Kommentar-Formulare und nutze gemeinsam mit dem Addon "YForm Usability" die Funktionalität, Kommentare freizugeben oder zu sperren. 
+
+> Tipp: Nutze zusätzlich das Spamschutz-Addon für die effektive Blockierung von Spam.
 
 ### Vorgefertigte Module und Fragmente
 
--- install.php
+++ install.php
@@ -6,7 +6,10 @@ if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
     rex_yform_manager_table::deleteCache();
 }
 
-rex_metainfo_add_field('translate:wortpresse_category_meta_category', 'cat_wortpresse_category', '100', 'multiple="multiple" perm="wortpresse[category_meta]" note="translate:wortpresse_category"', 3, '', 'SELECT name, id FROM rex_wortpresse_category ORDER BY name DESC');
-rex_metainfo_add_field('translate:wortpresse_category_meta_taxonomy', 'cat_wortpresse_taxonomy', '100', 'multiple="multiple" perm="wortpresse[category_meta]" note="translate:wortpresse_taxonomy"', 3, '', 'SELECT name, id FROM rex_wortpresse_taxonomy ORDER BY name DESC');
-rex_metainfo_add_field('translate:wortpresse_article_meta_category', 'art_wortpresse_category', '100', 'multiple="multiple" perm="wortpresse[article_meta]" note="translate:wortpresse_category"', 3, '', 'SELECT name, id FROM rex_wortpresse_category  ORDER BY name DESC');
-rex_metainfo_add_field('translate:wortpresse_article_meta_taxonomy', 'art_wortpresse_taxonomy', '100', 'multiple="multiple" perm="wortpresse[article_meta]" note="translate:wortpresse_taxonomy"', 3, '', 'SELECT name, id FROM rex_wortpresse_taxonomy ORDER BY name DESC');
+rex_metainfo_add_field('translate:wortpresse_category_meta_category', 'cat_wortpresse_category', '100', 'multiple="multiple" perm="wortpresse[category_meta]"', 3, '', 'SELECT name, id FROM rex_wortpresse_category ORDER BY name ASC');
+rex_metainfo_add_field('translate:wortpresse_category_meta_taxonomy', 'cat_wortpresse_taxonomy', '100', 'multiple="multiple" perm="wortpresse[category_meta]"', 3, '', 'SELECT name, id FROM rex_wortpresse_taxonomy ORDER BY name ASC');
+rex_metainfo_add_field('translate:wortpresse_article_meta_category', 'art_wortpresse_category', '100', 'multiple="multiple" perm="wortpresse[article_meta]"', 3, '', 'SELECT name, id FROM rex_wortpresse_category  ORDER BY name ASC');
+rex_metainfo_add_field('translate:wortpresse_article_meta_taxonomy', 'art_wortpresse_taxonomy', '100', 'multiple="multiple" perm="wortpresse[article_meta]"', 3, '', 'SELECT name, id FROM rex_wortpresse_taxonomy ORDER BY name ASC');
+rex_metainfo_add_field('translate:wortpresse_article_meta_author', 'art_wortpresse_author', '100', 'multiple="multiple" perm="wortpresse[article_author]"', 3, '', 'SELECT name, id FROM rex_user ORDER BY name ASC');
+rex_metainfo_add_field('translate:wortpresse_article_meta_publishdate', 'art_wortpresse_publishdate', '100', 'type="datetime-local" perm="wortpresse[article_publishdate]"', 1, '', '');
+rex_metainfo_add_field('translate:wortpresse_article_meta_comment', 'art_wortpresse_comment', '100', 'perm="wortpresse[article_comment]"', 5, '', '');
