4c10f6825c4553169a52b0696b1a7ab00f966aec
Alexander Walther <aw@alexplus.de>
2022-12-20 13:02:47 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -13,11 +13,24 @@ if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
         wp_menu::class
     );
 }
-
-if (rex::isBackend() && rex_be_controller::getCurrentPage() == "wortpresse/edit" || rex_be_controller::getCurrentPage() == "yform/manager/data_edit") {
-    rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep) {
-        $suchmuster = 'class="###wortpresse-settings-editor###"';
-        $ersetzen = rex_config::get("wortpresse", "editor");
-        $ep->setSubject(str_replace($suchmuster, $ersetzen, $ep->getSubject()));
-    });
+if (rex::isBackend() && is_object(rex::getUser())) {
+    rex_perm::register('wortpresse[category_meta]', "Kategorie-Taxonomien", rex_perm::EXTRAS);
+    rex_perm::register('wortpresse[article_meta]', "Artikel-Taxonomien", rex_perm::EXTRAS);
+    rex_perm::register('wortpresse[article_author]', "Autor/en", rex_perm::EXTRAS);
+    rex_perm::register('wortpresse[article_publishdate]', "Veröfentlichungsdatum", rex_perm::EXTRAS);
+    rex_perm::register('wortpresse[article_comment]', "Kommentare", rex_perm::EXTRAS);
 }
+
+# Prüfen, ob ein anderes Addon installiert ist, bspw. Cronjob-Addon
+/*
+if (rex_addon::get('cronjob')->isAvailable() && !rex::isSafeMode()) {
+    rex_cronjob_manager::registerType('rex_cronjob_wortpresse');
+}
+*/
+
+# Beim Extension Point REX_YFORM_SAVED etwas ausführen
+/*
+rex_extension::register('REX_YFORM_SAVED', function (rex_extension_point $ep) {
+    // Mein Code, oder meine Funktion / statische Methode aufrufen
+});
+*/
